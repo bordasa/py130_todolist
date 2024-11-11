2d386c546ed680623e28f209f3cf0fe4c86ce082
Alexei Bordas <alexei.bordas@gmail.com>
2024-11-10 20:27:46 -0800

    Working through examples of creating generators.

---
+++ lesson1/assignment10.py
@@ -0,0 +1,15 @@
+# reciprocals = (1/num for num in range(1,11))
+
+# for recip in reciprocals:
+#     print(recip)
+
+# def reciprocals(n):
+#     num = 1
+
+#     while num <= n:
+#         yield 1/num
+#         num += 1
+
+# for value in reciprocals(5):
+#     print(value)
+
