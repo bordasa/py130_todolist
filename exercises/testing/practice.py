4207b1f7a57d25981f6abc150f6df9d95779a3fd
Alexei Bordas <alexei.bordas@gmail.com>
2024-11-21 15:10:00 -0800

    Completed the Testing Practice Problems.

---
+++ exercises/testing/practice.py
@@ -0,0 +1,48 @@
+# Problem 1
+# Boolean Assertions
+
+import unittest
+
+# class TestPractice(unittest.TestCase):
+#     def setup(self):
+#         pass
+
+#     def test_odd_numbers_only(self):
+#         value = 5
+#         self.assertTrue(value % 2 != 0, 'value is not odd')
+
+# Problem 2
+# Equality Assertions
+
+# self.assertEqual('xyz', value.lower())
+
+# Problem 3
+# None Assertions
+
+# self.assertIsNone(value)
+
+# Problem 4
+# Included Object Assertions
+
+# self.assertIn('xyz', lst)
+
+# Problem 5
+# Not Included Object Assertions
+
+# self.assertNotIn('xyz', lst)
+
+# Problem 6
+# Exception Assertions
+
+# with self.assertRaises(NoExperienceError):
+    # employee.hire()
+
+# Problem 7
+# Type Assertions
+
+# self.assertIsInstance(value, Numeric)
+
+# Problem 8
+# Same Object Assertions
+
+# self.assertIs(lst, lst.process())
